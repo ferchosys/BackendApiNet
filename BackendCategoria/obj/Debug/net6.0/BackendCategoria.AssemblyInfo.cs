161529650fadb5296bd8ae79ad3afd5eb2fa1405
jose aguilar <jlaguilarc@misena.edu.co>
2024-04-01 12:43:23 -0500

    configuracion nconexion DB de Api net c#

--- BackendCategoria/obj/Debug/net6.0/BackendCategoria.AssemblyInfo.cs
+++ BackendCategoria/obj/Debug/net6.0/BackendCategoria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendCategoria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b81127f1769ca646fecf2cdbde739f5296515e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendCategoria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendCategoria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
